aaaff1a80f9af5ea8a2b83fc039a3586b78e1a3a
SmartWerk-maker <smartwerk.team@gmail.com>
2026-03-26 21:13:22 +0000

    fix: allow null stripe instance

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from "stripe";
 
 let stripeInstance: Stripe | null = null;
 
-export function getStripe(): Stripe {
+export function getStripe(): Stripe | null {
   if (stripeInstance) return stripeInstance;
 
   const key = process.env.STRIPE_SECRET_KEY;
